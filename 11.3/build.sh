0ad1cf44dd8de115d7c42a8569ee0700a6658564
Luciano Ferreira <lucianovg.ferreira@gmail.com>
2025-08-31 11:33:32 -0400

    Primeiro commit

---
+++ 11.3/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Exit script on error
+set -e
+
+# Variables for image name and tags
+IMAGE_NAME="lucianovg/paserver-dfe"
+TAG_LATEST="latest"
+TAG_VERSION="alexandria"
+TAG_NUMERIC="11.3.0"
+
+# Build the Docker image with the first tag
+docker build . \
+        --build-arg password=securepass \
+        --platform linux/amd64 \
+		--pull \
+		-t ${IMAGE_NAME}:${TAG_LATEST} \
+
+# Tag the built image with additional version tags
+docker tag "${IMAGE_NAME}:${TAG_LATEST}" "${IMAGE_NAME}:${TAG_VERSION}"
+docker tag "${IMAGE_NAME}:${TAG_LATEST}" "${IMAGE_NAME}:${TAG_NUMERIC}"
+
+# Echo a success message
+echo "Docker image ${IMAGE_NAME} tagged with ${TAG_LATEST}, ${TAG_VERSION}, and ${TAG_NUMERIC}"
---
+++ 11.3/paserver_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+./paserver -password=$PA_SERVER_PASSWORD
+status=$?
+if [ $status -ne 0 ]; then
+	echo "Failed to start paserver: $status"
+	exit $status
+fi
+
+while sleep 10; do
+	ps aux |grep paserver |grep -q -v grep
+	PA_SERVER_STATUS=$?
+	echo $PA_SERVER_STATUS
+	if [ $PA_SERVER_STATUS -eq 0 ]; then
+		echo "Complete!"
+		exit 1
+	fi
+done
